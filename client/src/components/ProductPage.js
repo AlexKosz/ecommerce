fb249f504772da2bf28931e3400a25a128391c24
Alex Koszuta <alex@blockbustercostumes.com>
2022-02-14 15:21:47 -0500

    Start of product page

--- client/src/components/ProductPage.js
+++ client/src/components/ProductPage.js
@@ -1,36 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './styles/ProductPage.css';
+import './styles/ProductPage.scss';
 import { useParams } from '@reach/router';
+import Stars from './Stars';
 
-
-const baseURL = 'http://localhost:8000'
-const apiURL = `${baseURL}/api`
-
-
-const ProductPage = () => {
-
-
+const ProductPage = (props) => {
+    const apiURL = props.urls.apiURL
+    const baseURL = props.urls.baseURL
     const { productId } = useParams()
     const [product, setProduct] = useState([])
 
-    // const getProduct = () => {
-    //     axios.get(`${apiURL}/products/`).then(res => {
-    //         setProduct(res.data)
-    //         console.log(res.data);
-    //     })
-    // }
+    const getProduct = () => {
+        axios.get(`${apiURL}/product/${productId}`).then(res => {
+            setProduct(res.data)
+            console.log(res.data);
+        })
+    }
 
-    // useEffect(() => {
-    //     getProduct()
-    // }, [])
+    useEffect(() => {
+        getProduct()
+    }, [])
 
-
-
+    const percent = product.average_star_rating * 20
 
     return (
         <div className='mainProductPage'>
-            <h3>{productId}</h3>
+            <a href="/">Back to main page</a>
+            <div className='mainProductWrapper'>
+                <img src = {`${baseURL}${product.image}`} />
+                <div className='productInfo'>
+                    <h1>{product.name}</h1>
+                    <Stars rating={product.average_star_rating}/>
+                </div>
+                <div className='productActions'>
+                    <button>Add to cart</button>
+                </div>
+                
+            </div>
+
         </div>
     );
 }
---
+++ client/src/components/Stars.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Stars = (props) => {
+    const stars = Math.floor(props.rating)
+    const empty = 5 - stars
+    return (
+        <div className='starsWrapper'>
+            
+        </div>
+
+    );
+
+
+
+
+
+}
+
+export default Stars;
